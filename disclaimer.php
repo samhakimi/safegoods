f7b845d7eafddab0972c44f02403b8c089aec733
samhakimi <sam.hakimi2@gmail.com>
2014-05-23 11:34:51 -0400

    off to Github

---
+++ disclaimer.php
@@ -0,0 +1,2 @@
+<h3 class="widget-title">Disclaimer</h3>			<div class="textwidget">CPSC does not guarantee the accuracy, completeness, or adequacy of the contents of the Publicly Available Consumer Product Safety Information Database on SaferProducts.gov, particularly with respect to information submitted by persons or entities outside of CPSC.</div>
+		
