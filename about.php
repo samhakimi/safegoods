5e59deb76fce6bfe7cb98bc208746bfc4f81dca4
sam hakimi <sam.hakimi2@gmail.com>
2014-06-09 11:24:48 -0400

    fix button action

--- about.php
+++ about.php
@@ -23,29 +23,36 @@ Feedback and Suggestions Welcome
     <input type="email" name="email" id="email" value="" class="ui-input-text ui-body-c ui-corner-all ui-shadow-inset">
 
     <label for="textarea">Feedback:</label>
-    <textarea cols="40" rows="8" name="feedback" id="feedback"></textarea>
-    <button type="submit" data-theme="a" class="ui-btn-hidden" aria-disabled="false">Submit</button> 
+    <textarea cols="40" rows="2" name="feedback" id="feedback"></textarea>
+    <button type="button" id="feedbackButton" data-theme="a" class="ui-btn-hidden" aria-disabled="false">Submit</button> 
     </form>
+
+
+
 <script>
-$("#feedbackForm").submit(function() {
-      event.preventDefault();
-     // console.log( $( this ).serialize() );
-
-    var url = "./about_savefeedback.php"; 
-
-    $.ajax({
+$("#feedbackButton").click(function() {
+     //event.preventDefault();
+      
+     var url = "./about_savefeedback.php"; 
+     $.ajax({
            type: "GET",
            url: url,
-           data: $("#feedbackForm").serialize(), // serializes the form's elements.
-           success: function(data)
-           {
-               $('#feedbackForm').html('Thank you!');
-               console.log(data); // show response from the php script.
-           }
-         });
-
-    return false; // avoid to execute the actual submit of the form.
+           data: $("#feedbackForm").serialize() // serializes the form's elements.
+      })
+      .done(function( data ) {
+           $('#feedbackForm').html('Thank you!');
+      }) 
+      .fail(function() {
+            console.log( "error" );
+      })
+      .always(function() {
+             console.log( "complete" );
+      });;
+       
 });
 </script> 
+
+
+
 </div> 
 </div> 
